8d528fbaddfd2fa199e98ad380bcb2937d3896a1
Peter Williams <peter.williams@papercut.com>
2019-08-14 09:20:01 +1000

    Cleaned up nlmeans_test.py

--- denoise/nlm/nlmeans_test.py
+++ denoise/nlm/nlmeans_test.py
@@ -7,70 +7,87 @@ import numpy as np
 def main():
     assert len(argv) == 3, (len(argv), argv)
     origPath = argv[1]
-    scanBase = argv[2]
+    scanPath = argv[2]
     orig = cv2.imread(origPath)
+    scan_ = cv2.imread(scanPath)
+    cv2.imwrite("orig.png", orig)
+    cv2.imwrite("scan.png", scan_)
+    print("origPath=%s" % origPath)
+    print("scanPath=%s" % scanPath)
 
-    denoisedScans = nlmeans(scanBase)
+    denoisedScans = nlmeans(scanPath, origPath)
     results = []
     for denoisedPath, templSize, searchSize in denoisedScans:
         print("-" * 80)
         print(denoisedPath, templSize, searchSize)
-        scan = cv2.imread(denoisedPath)
-        scanAligned = matchAffine(orig, scan)
+        denoised = cv2.imread(denoisedPath)
+        for gauss in (3, 5, 15, 25, 35):
+            scanAligned = matchAffine(orig, denoised, gauss, number_of_iterations=1000)
 
-        # convert the images to grayscale
-        grayOrig = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
-        grayScan = cv2.cvtColor(scanAligned, cv2.COLOR_BGR2GRAY)
-        grayOrig = contract(grayOrig, 0.05)
-        grayScan = contract(grayScan, 0.05)
+            alignedPath = imageName("align%02d" % gauss, templSize, searchSize)
+            diffPath = imageName("diff%02d" % gauss, templSize, searchSize)
+            cv2.imwrite(alignedPath, scanAligned)
 
-        # compute the Structural Similarity Index (SSIM) between the two
-        # images, ensuring that the difference image is returned
-        score, diff = compare_ssim(grayOrig, grayScan, full=True)
-        diff = (diff * 255).astype("uint8")
+            # convert the images to grayscale
+            grayOrig = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+            grayScan = cv2.cvtColor(scanAligned, cv2.COLOR_BGR2GRAY)
+            grayOrig = contract(grayOrig, 0.05)
+            grayScan = contract(grayScan, 0.05)
 
-        print("grayOrig=%s" % list(grayOrig.shape))
-        print("grayScan=%s" % list(grayScan.shape))
-        print("diff=%s" % list(diff.shape))
-        print("SSIM: %s" % score)
+            # compute the Structural Similarity Index (SSIM) between the two
+            # images, ensuring that the difference image is returned
+            score, diff = compare_ssim(grayOrig, grayScan, full=True)
+            diff = (diff * 255).astype("uint8")
 
-        results.append((score, templSize, searchSize, denoisedPath))
+            print("gauss=%d" % gauss)
+            print("grayOrig=%s" % list(grayOrig.shape))
+            print("grayScan=%s" % list(grayScan.shape))
+            print("diff=%s" % list(diff.shape))
+            print("SSIM: %s" % score)
+
+            cv2.imwrite(diffPath, diff)
+
+            results.append((score, gauss, templSize, searchSize, denoisedPath))
 
     results.sort(key=lambda x: (-x[0], x[1], x[2]))
     print("=" * 80)
     for i, r in enumerate(results):
-        score, templSize, searchSize, denoisedPath= r
-        print("%d: %.3f %2d x %2d %s" % (i,  score, templSize, searchSize, denoisedPath))
+        score, gauss, templSize, searchSize, denoisedPath= r
+        print("%d: %.3f [%2d] %2d x %2d %s" %
+              (i, score, gauss, templSize, searchSize, denoisedPath))
 
 
-def nlmeans(inPath):
-    name = os.path.basename(inPath)
+def nlmeans(scanPath, origPath):
+    name = os.path.basename(scanPath)
     name, _ = os.path.splitext(name)
-    color_path = '%s_color.png' % name
-    gray_path = '%s_gray.png' % name
+    # color_path = '%s_scan_color.png' % name
+    # gray_path = '%s_scan_gray.png' % name
 
-    image = cv2.imread(inPath, cv2.IMREAD_COLOR)
-    print(type(image))
+    image = cv2.imread(scanPath, cv2.IMREAD_COLOR)
+    # print(type(image))
     print("%s:%s" % (image.shape, image.dtype))
     # assert image, inPath
-    source_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(color_path, image)
-    cv2.imwrite(gray_path, source_gray)
+    # source_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # cv2.imwrite(color_path, image)
+    # cv2.imwrite(gray_path, source_gray)
 
-    results = [(inPath, 0, 0)]
+    results = [(scanPath, 0, 0), (origPath, -1, -1)]
 
     # for templSize in (7, 9, 13, 15):
     #     for searchSize in (11, 13, 21, 27):
-    for templSize in reversed([25, 27, 29, 26]):
-        for searchSize in reversed([105]):
-            print("nlMeans", inPath, templSize, searchSize)
-            denoisedPath = nlmeansParams(name, image, templSize, searchSize)
+    # for templSize in reversed([29, 33]):
+    #     for searchSize in reversed([105]):
+    for templSize in reversed([7]):
+        for searchSize in reversed([21]):
+            denoisedPath = imageName('%s_denoised' % name, templSize, searchSize)
+            print("nlMeans", scanPath, templSize, searchSize, denoisedPath)
+            denoised = nlmeansParams(name, image, templSize, searchSize)
+            cv2.imwrite(denoisedPath, denoised)
             results.append((denoisedPath, templSize, searchSize))
     return results
 
 
 def nlmeansParams(name, image, templSize, searchSize):
-    denoisedPath = '%s_denoised_%dx%d.png' % (name, templSize, searchSize)
     # denoised_gray_path = '%s_denoised_gray_%dx%d.png' % (
     #     name, templSize, searchSize)
     # denoised_gray = cv2.fastNlMeansDenoising(source_gray, None,
@@ -81,20 +98,16 @@ def nlmeansParams(name, image, templSize, searchSize):
     denoised = cv2.fastNlMeansDenoisingColored(image, None,
                                                 templateWindowSize=templSize,
                                                 searchWindowSize=searchSize)
-    cv2.imwrite(denoisedPath, denoised)
-    return denoisedPath
+    return denoised
 
 
-def matchAffine(orig, scan):
+def matchAffine(orig, scan, gauss, number_of_iterations=100, warp_mode=cv2.MOTION_EUCLIDEAN):
     # Convert images to grayscale
     origGray = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
     scanGray = cv2.cvtColor(scan, cv2.COLOR_BGR2GRAY)
 
     # Find size of image1
-    sz = origGray.shape
-
-    # Define the motion model
-    warp_mode = cv2.MOTION_AFFINE
+    h, w = origGray.shape[:2]
 
     # Define 2x3 or 3x3 matrices and initialize the matrix to identity
     if warp_mode == cv2.MOTION_HOMOGRAPHY:
@@ -102,7 +115,6 @@ def matchAffine(orig, scan):
     else:
         warp_matrix = np.eye(2, 3, dtype=np.float32)
 
-    number_of_iterations = 100
     # Specify the threshold of the increment
     # in the correlation coefficient between two iterations
     termination_eps = 1e-10
@@ -113,28 +125,26 @@ def matchAffine(orig, scan):
 
     # Run the ECC algorithm. The results are stored in warp_matrix.
     # print("before")
-    cc, warp_matrix = cv2.findTransformECC(origGray, scanGray, warp_matrix, warp_mode, criteria,
-                                           inputMask=None, gaussFiltSize=5)
+    cc, warp_matrix = cv2.findTransformECC(origGray, scanGray, warp_matrix, motionType=warp_mode,
+                                           criteria=criteria,
+                                           inputMask=None, gaussFiltSize=gauss)
     # print("after")
     print("warp_matrix=%s" % warp_matrix)
+    flags = cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP
     if warp_mode == cv2.MOTION_HOMOGRAPHY:
         # Use warpPerspective for Homography
-        im2_aligned = cv2.warpPerspective(
-            scan, warp_matrix, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        scanAligned = cv2.warpPerspective(scan, warp_matrix, (w, h), flags=flags)
         assert False
     else:
         # Use warpAffine for Translation, Euclidean and Affine
-        im2_aligned = cv2.warpAffine(
-            scan, warp_matrix, (sz[1], sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        scanAligned = cv2.warpAffine(scan, warp_matrix, (w, h), flags=flags)
 
     # Show final results
-    cv2.imwrite("orig.png", orig)
-    cv2.imwrite("scan.png", scan)
-    cv2.imwrite("scan.aligned.png", im2_aligned)
+
     print("orig=%s" % list(orig.shape))
     print("scan=%s" % list(scan.shape))
-    print("scan.aligned=%s" % list(im2_aligned.shape))
-    return im2_aligned
+    print("scan.aligned=%s" % list(scanAligned.shape))
+    return scanAligned
 
 
 def contract(image, frac):
@@ -142,6 +152,10 @@ def contract(image, frac):
     dx = int(w * frac)
     dy = int(h * frac)
     return image[dy:h-dy, dx:w-dx]
+
+
+def imageName(name, templSize, searchSize):
+    return '%s_%dx%d.png' % (name, templSize, searchSize)
 
 
 main()
